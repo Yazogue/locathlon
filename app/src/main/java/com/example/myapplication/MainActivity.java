487799ad4071e0da16e9e0cf5965988e9ace1023
Samuel <samuel.dcostar@gmail.com>
2023-11-26 19:51:57 +0100

    v2.4 first part done

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -50,6 +50,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initRecyclerView();
+
         ViewPager2 viewPager = findViewById(R.id.viewPager);
         viewPager.setAdapter(new MyPagerAdapter(this));
 
