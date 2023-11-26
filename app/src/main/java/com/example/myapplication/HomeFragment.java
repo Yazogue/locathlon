09480f3d969cd464ffa8f06cd28d99d6e6950454
Samuel <samuel.dcostar@gmail.com>
2023-11-26 20:23:13 +0100

    v2.4 first part done

--- app/src/main/java/com/example/myapplication/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/HomeFragment.java
@@ -7,24 +7,45 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
+    private RecyclerView.Adapter adapterArticles;
+    private RecyclerView recyclerViewArticles;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        View rootView =  inflater.inflate(R.layout.fragment_home, container, false);
+        initRecyclerView(rootView);
+        return rootView;
     }
 
-    // Méthode dans le fragment où vous souhaitez appeler la méthode de MainActivity
-    private void appelDeMethodeDansMainActivity() {
-        // Obtenir une référence à l'activité parente (MainActivity)
-        MainActivity mainActivity = (MainActivity) getActivity();
+    public void initRecyclerView(View rootView) {
+        ArrayList<ArticlesDomain> items=new ArrayList<>();
+        items.add(new ArticlesDomain("VTT BTWIN 6-9ans","Bonjour, je loue ce VTT Racing Boy de BTWIN, parfait pour les enfants de 6 à 9 ans.\" +\n" +
+                "                \" Robuste, avec un design inspiré des vélos de course, il offre une expérience de conduite dynamique.\" +\n" +
+                "                \" Facile à manœuvrer et à entretenir, il est prêt à accompagner votre petit aventurier sur tous les terrains. \" +\n" +
+                "                \"En excellent état, prêt à rouler. Ne manquez pas cette opportunité !","btwin_racing_boy_vtt_200x133.jpg", 15, 20, 69));
+        items.add(new ArticlesDomain("VTT TEST2","Bonjour, je loue ce VTT Racing Boy de BTWIN, parfait pour les enfants de 6 à 9 ans.\" +\n" +
+                "                \" Robuste, avec un design inspiré des vélos de course, il offre une expérience de conduite dynamique.\" +\n" +
+                "                \" Facile à manœuvrer et à entretenir, il est prêt à accompagner votre petit aventurier sur tous les terrains. \" +\n" +
+                "                \"En excellent état, prêt à rouler. Ne manquez pas cette opportunité !","btwin_racing_boy_vtt_200x133.jpg", 15, 20, 150));
+        items.add(new ArticlesDomain("VTT TEST3","Bonjour, je loue ce VTT Racing Boy de BTWIN, parfait pour les enfants de 6 à 9 ans.\" +\n" +
+                "                \" Robuste, avec un design inspiré des vélos de course, il offre une expérience de conduite dynamique.\" +\n" +
+                "                \" Facile à manœuvrer et à entretenir, il est prêt à accompagner votre petit aventurier sur tous les terrains. \" +\n" +
+                "                \"En excellent état, prêt à rouler. Ne manquez pas cette opportunité !","btwin_racing_boy_vtt_200x133.jpg", 37, 10, 20));
 
-        // Vérifier si l'activité est non nulle avant d'appeler la méthode
-        if (mainActivity != null) {
-            // Appeler la méthode de MainActivity
-            mainActivity.initRecyclerView();
+        recyclerViewArticles=rootView.findViewById(R.id.view1);
+
+        if (recyclerViewArticles != null) {
+            recyclerViewArticles.setLayoutManager(new LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false));
+            adapterArticles = new ArticlesListAdapter(items);
+            recyclerViewArticles.setAdapter(adapterArticles);
         }
     }
-
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -18,39 +18,10 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    private RecyclerView.Adapter adapterArticles;
-    private RecyclerView recyclerViewArticles;
-
-    public void initRecyclerView() {
-        ArrayList<ArticlesDomain> items=new ArrayList<>();
-        items.add(new ArticlesDomain("VTT BTWIN 6-9ans","Bonjour, je loue ce VTT Racing Boy de BTWIN, parfait pour les enfants de 6 à 9 ans.\" +\n" +
-                "                \" Robuste, avec un design inspiré des vélos de course, il offre une expérience de conduite dynamique.\" +\n" +
-                "                \" Facile à manœuvrer et à entretenir, il est prêt à accompagner votre petit aventurier sur tous les terrains. \" +\n" +
-                "                \"En excellent état, prêt à rouler. Ne manquez pas cette opportunité !","btwin_racing_boy_vtt_200x133.jpg", 15, 20, 69));
-        items.add(new ArticlesDomain("VTT TEST2","Bonjour, je loue ce VTT Racing Boy de BTWIN, parfait pour les enfants de 6 à 9 ans.\" +\n" +
-                "                \" Robuste, avec un design inspiré des vélos de course, il offre une expérience de conduite dynamique.\" +\n" +
-                "                \" Facile à manœuvrer et à entretenir, il est prêt à accompagner votre petit aventurier sur tous les terrains. \" +\n" +
-                "                \"En excellent état, prêt à rouler. Ne manquez pas cette opportunité !","btwin_racing_boy_vtt_200x133.jpg", 15, 20, 150));
-        items.add(new ArticlesDomain("VTT TEST3","Bonjour, je loue ce VTT Racing Boy de BTWIN, parfait pour les enfants de 6 à 9 ans.\" +\n" +
-                "                \" Robuste, avec un design inspiré des vélos de course, il offre une expérience de conduite dynamique.\" +\n" +
-                "                \" Facile à manœuvrer et à entretenir, il est prêt à accompagner votre petit aventurier sur tous les terrains. \" +\n" +
-                "                \"En excellent état, prêt à rouler. Ne manquez pas cette opportunité !","btwin_racing_boy_vtt_200x133.jpg", 37, 10, 20));
-
-        recyclerViewArticles=findViewById(R.id.view1);
-        recyclerViewArticles.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
-
-        adapterArticles=new ArticlesListAdapter(items);
-        recyclerViewArticles.setAdapter(adapterArticles);
-
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        initRecyclerView();
 
         ViewPager2 viewPager = findViewById(R.id.viewPager);
         viewPager.setAdapter(new MyPagerAdapter(this));
