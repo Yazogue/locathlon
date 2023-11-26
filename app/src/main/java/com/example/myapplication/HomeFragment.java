60658613075c081f5e7e1ce59c0a63a9c29852a0
Samuel <samuel.dcostar@gmail.com>
2023-11-26 20:43:40 +0100

    v2.4 first part done

--- app/src/main/java/com/example/myapplication/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/HomeFragment.java
@@ -24,21 +24,20 @@ public class HomeFragment extends Fragment {
         initRecyclerView(rootView);
         return rootView;
     }
-
     public void initRecyclerView(View rootView) {
         ArrayList<ArticlesDomain> items=new ArrayList<>();
         items.add(new ArticlesDomain("VTT BTWIN 6-9ans","Bonjour, je loue ce VTT Racing Boy de BTWIN, parfait pour les enfants de 6 à 9 ans.\" +\n" +
                 "                \" Robuste, avec un design inspiré des vélos de course, il offre une expérience de conduite dynamique.\" +\n" +
                 "                \" Facile à manœuvrer et à entretenir, il est prêt à accompagner votre petit aventurier sur tous les terrains. \" +\n" +
-                "                \"En excellent état, prêt à rouler. Ne manquez pas cette opportunité !","btwin_racing_boy_vtt_200x133.jpg", 15, 20, 69));
+                "                \"En excellent état, prêt à rouler. Ne manquez pas cette opportunité !","@drawable"+ "/" + "btwin_racing_boy_vtt_200x133.jpg", 15, 20, 69));
         items.add(new ArticlesDomain("VTT TEST2","Bonjour, je loue ce VTT Racing Boy de BTWIN, parfait pour les enfants de 6 à 9 ans.\" +\n" +
                 "                \" Robuste, avec un design inspiré des vélos de course, il offre une expérience de conduite dynamique.\" +\n" +
                 "                \" Facile à manœuvrer et à entretenir, il est prêt à accompagner votre petit aventurier sur tous les terrains. \" +\n" +
-                "                \"En excellent état, prêt à rouler. Ne manquez pas cette opportunité !","btwin_racing_boy_vtt_200x133.jpg", 15, 20, 150));
+                "                \"En excellent état, prêt à rouler. Ne manquez pas cette opportunité !","@drawable"+ "/" + "btwin_racing_boy_vtt_200x133.jpg", 15, 20, 150));
         items.add(new ArticlesDomain("VTT TEST3","Bonjour, je loue ce VTT Racing Boy de BTWIN, parfait pour les enfants de 6 à 9 ans.\" +\n" +
                 "                \" Robuste, avec un design inspiré des vélos de course, il offre une expérience de conduite dynamique.\" +\n" +
                 "                \" Facile à manœuvrer et à entretenir, il est prêt à accompagner votre petit aventurier sur tous les terrains. \" +\n" +
-                "                \"En excellent état, prêt à rouler. Ne manquez pas cette opportunité !","btwin_racing_boy_vtt_200x133.jpg", 37, 10, 20));
+                "                \"En excellent état, prêt à rouler. Ne manquez pas cette opportunité !","@drawable"+ "/" + "btwin_racing_boy_vtt_200x133.jpg", 37, 10, 20));
 
         recyclerViewArticles=rootView.findViewById(R.id.view1);
 
